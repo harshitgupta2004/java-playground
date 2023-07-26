de5e8c9b40a954fb3960472ba74c14b06d31836b
harshitgupta2004 <harshitcba@gmail.com>
2023-07-26 11:27:08 +0530

    update

--- ARRAYLIST/Qs_Exercise11_2.java
+++ ARRAYLIST/Qs_Exercise11_2.java
@@ -10,7 +10,7 @@ public class Qs_Exercise11_2 {
         Collections.sort(nums);
         ArrayList<Integer> list = new ArrayList<>();
         for(int i = 1; i < nums.size()-1; i++) {
-            if(nums.get(i-1) < nums.get(i) && nums.get(i) + 1 < nums.get(i+1)) {
+            if(nums.get(i-1) + 1 < nums.get(i) && nums.get(i) + 1 < nums.get(i+1)) {
                 list.add(nums.get(i));
             }
         }
@@ -19,7 +19,7 @@ public class Qs_Exercise11_2 {
             list.add(nums.get(0));
         }
         if(nums.size() > 1) {
-            if (nums.size() > 1){
+            if (nums.get(0) + 1 < nums.get(1)){
                 list.add(nums.get(0));
             }
             if(nums.get(nums.size()-2) + 1 < nums.get(nums.size()-1)) {
