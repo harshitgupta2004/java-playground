2ff1483f66c4eb2494efcdbbc2eeffc20383d807
harshitgupta2004 <harshitcba@gmail.com>
2023-07-27 17:32:12 +0530

    updated solution

--- RECURSION/Problem3.java
+++ RECURSION/Problem3.java
@@ -7,12 +7,12 @@ public class Problem3 {
         if(n == 0) {
             return 1; //0! = 1
         }
-        int fnm1 = fact(n-1); // calculating n-1 factorial
+        // int fnm1 = fact(n-1); // calculating n-1 factorial
         int fn = n * fact(n-1); //calc n factorial
         return fn;
     }
     public static void main(String args[]) {
-        int n = 5;
+        int n = 10;
         System.out.println(fact(n));
     }
 }
--- RECURSION/Problem4.java
+++ RECURSION/Problem4.java
@@ -7,7 +7,7 @@ public class Problem4 {
             return 1;
         }
         
-        int Snm1 = calcSum(n-1); //calc sum of n-1 nos
+        // int Snm1 = calcSum(n-1); //calc sum of n-1 nos
         int Sn = n + calcSum(n-1); //calc sum of n nos
         return Sn;
     }
