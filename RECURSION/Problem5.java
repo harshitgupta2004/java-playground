1cf356a0639b080bf340f8188e9cc5c2c5b3e218
harshitgupta2004 <harshitcba@gmail.com>
2023-07-29 09:43:54 +0530

    changes

--- RECURSION/Problem5.java
+++ RECURSION/Problem5.java
@@ -7,9 +7,9 @@ public class Problem5 {
         if(n == 0 || n == 1){
             return n;
         }
-        int fnm1 = fibonacci(n-1);
-        int fnm2 = fibonacci(n-2);
-        int fn = fnm1 + fnm2;
+        // int fnm1 = fibonacci(n-1);
+        // int fnm2 = fibonacci(n-2);
+        int fn = fibonacci(n-1) + fibonacci(n-2);
         return fn;
     }
     public static void main(String args[]) {
