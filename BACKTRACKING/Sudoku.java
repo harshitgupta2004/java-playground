42fc9014b2396d4bc86bc595803c520ab4a41d5c
harshitgupta2004 <harshitcba@gmail.com>
2023-07-29 09:43:27 +0530

    changes in code

--- BACKTRACKING/Sudoku.java
+++ BACKTRACKING/Sudoku.java
@@ -64,7 +64,7 @@ public class Sudoku {
     }
 
     public static void printSudoku(int sudoku[][]) {
-        for(int i = 0; i <= 9; i++) {
+        for(int i = 0; i < 9; i++) {
             for(int j = 0; j < 9; j++) {
                 System.out.print(sudoku[i][j]+ " ");
             }
