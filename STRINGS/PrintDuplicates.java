e584c69799282dee6f29862cdcaa9f8cf8044245
harshitgupta2004 <harshitcba@gmail.com>
2023-08-01 10:18:51 +0530

    Print all the duplicates in the input string

---
+++ STRINGS/PrintDuplicates.java
@@ -0,0 +1,19 @@
+public class PrintDuplicates {
+    public static void printDups(String s){
+        int count[] = new int[256];
+
+        for(int i = 0 ; i < s.length(); i++){
+            count[s.charAt(i)]++;
+        }
+
+        for(int i = 0; i < 256; i++){
+            if(count[i] > 1){
+                System.out.println((char)(i) + ", count = " + count[i]);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        String str = "test string";
+        printDups(str);
+    }
+}
