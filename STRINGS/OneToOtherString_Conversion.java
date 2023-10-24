a0534d97a98f4ecbdd10c2d997c76b6aa2c469d5
harshitgupta2004 <harshitcba@gmail.com>
2023-10-24 10:16:18 +0530

    fix formatting

--- STRINGS/OneToOtherString_Conversion.java
+++ STRINGS/OneToOtherString_Conversion.java
@@ -1,8 +1,8 @@
 /* Given two strings A and B, the task is to convert A to B if possible. The only operation allowed is to put any character from A and insert it at front. Find if it’s possible to convert the string. If yes, then output minimum no. of operations required for transformation */
 
 public class OneToOtherString_Conversion {
-    public static int minOps(String a, String b){
-        if(a.length() != b.length()){
+    public static int minOps(String a, String b) {
+        if (a.length() != b.length()) {
             return -1;
         }
 
@@ -10,25 +10,25 @@ public class OneToOtherString_Conversion {
         int count[] = new int[256];
 
         // count characters in A , subtract count for every character in B
-        for(i = 0; i < a.length(); i++){
+        for (i = 0; i < a.length(); i++) {
             count[a.charAt(i)]++;
             count[b.charAt(i)]--;
         }
 
         // check if all counts become 0
-        for(i = 0; i < 256; i++){
-            if(count[i] != 0){
+        for (i = 0; i < 256; i++) {
+            if (count[i] != 0) {
                 return -1;
             }
         }
 
-        i = a.length()-1;
-        j = b.length()-1;
+        i = a.length() - 1;
+        j = b.length() - 1;
 
-        while(i >= 0){
-            if(a.charAt(i) != b.charAt(j)){
+        while (i >= 0) {
+            if (a.charAt(i) != b.charAt(j)) {
                 res++;
-            }else{
+            } else {
                 j--;
             }
 
@@ -37,10 +37,11 @@ public class OneToOtherString_Conversion {
 
         return res;
     }
+
     public static void main(String[] args) {
         String A = "EACBD";
         String B = "EABCD";
- 
+
         System.out.println("Minimum number of operations required is " + minOps(A, B));
     }
 }
