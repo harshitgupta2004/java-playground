b9cab01dd58cbaede451b364d3be67a6f568179b
harshitgupta2004 <harshitcba@gmail.com>
2023-10-24 10:16:00 +0530

    format

--- STRINGS/MobileNumericKeypad_Seq.java
+++ STRINGS/MobileNumericKeypad_Seq.java
@@ -1,27 +1,27 @@
 public class MobileNumericKeypad_Seq {
-    public static String printSequence(String str[], String input){
+    public static String printSequence(String str[], String input) {
         String op = "";
         int n = input.length();
         input = input.toUpperCase();
 
-        for(int i = 0; i < n; i++){
-            if(input.charAt(i) == ' '){
+        for (int i = 0; i < n; i++) {
+            if (input.charAt(i) == ' ') {
                 op += 0;
-            }else{
+            } else {
                 int pos = input.charAt(i) - 'A';
                 op += str[pos];
             }
         }
         return op;
     }
+
     public static void main(String[] args) {
-        String str[]
-            = { "2",    "22",  "222", "3",   "33", "333",
-                "4",    "44",  "444", "5",   "55", "555",
-                "6",    "66",  "666", "7",   "77", "777",
-                "7777", "8",   "88",  "888", "9",  "99",
-                "999",  "9999" };
- 
+        String str[] = { "2", "22", "222", "3", "33", "333",
+                "4", "44", "444", "5", "55", "555",
+                "6", "66", "666", "7", "77", "777",
+                "7777", "8", "88", "888", "9", "99",
+                "999", "9999" };
+
         String input = "Hi I am Harshit";
         System.out.println(printSequence(str, input));
     }
--- STRINGS/RabinKarp_forPatternSearch.java
+++ STRINGS/RabinKarp_forPatternSearch.java
@@ -1,7 +1,7 @@
 public class RabinKarp_forPatternSearch {
     public final static int d = 256;
 
-    public static void search(String txt, String pat, int q){
+    public static void search(String txt, String pat, int q) {
         int n = txt.length();
         int m = pat.length();
         int i, j;
@@ -9,51 +9,54 @@ public class RabinKarp_forPatternSearch {
         int t = 0; // hash value for text
         int h = 1; // h = (d^m-1)%q
 
-        for(i = 0; i < m-1; i++){
-            h = (h*d)%q;
+        for (i = 0; i < m - 1; i++) {
+            h = (h * d) % q;
         }
 
         // Calculate the hash value of pattern and first window of text
-        for(i = 0; i < m; i++){
+        for (i = 0; i < m; i++) {
             p = (d * p + pat.charAt(i)) % q;
             t = (d * t + txt.charAt(i)) % q;
         }
 
         // Slide the pattern over text one by one
-        for(i = 0; i <= n-m; i++){
-            // Check the hash values of current window of text and pattern. If the hash values match
+        for (i = 0; i <= n - m; i++) {
+            // Check the hash values of current window of text and pattern. If the hash
+            // values match
             // then only check for characters one by one
-            if(p == t){
+            if (p == t) {
                 // check for chars one by one
-                for(j = 0; j < m; j++){
-                    if(txt.charAt(i+j) != pat.charAt(j)){
+                for (j = 0; j < m; j++) {
+                    if (txt.charAt(i + j) != pat.charAt(j)) {
                         break;
                     }
                 }
 
-                if(j == m){
+                if (j == m) {
                     System.out.println("Pattern found at index : " + i);
                 }
             }
 
-            // Calculate hash value for next window of text: Remove leading digit, add trailing digit
-            if(i < n-m){
-                t = (d * (t - txt.charAt(i) * h) + txt.charAt(i+m)) % q;
+            // Calculate hash value for next window of text: Remove leading digit, add
+            // trailing digit
+            if (i < n - m) {
+                t = (d * (t - txt.charAt(i) * h) + txt.charAt(i + m)) % q;
 
                 // we may get -ve value of t, so convert into +ve
-                if(t < 0){
+                if (t < 0) {
                     t = t + q;
                 }
             }
         }
     }
+
     public static void main(String[] args) {
         String txt = "AABAACAADAABAABA";
         String pat = "AABA";
- 
+
         // A prime number
         int q = 101;
- 
+
         search(txt, pat, q);
     }
 }
