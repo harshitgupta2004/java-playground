66374971a993279423a963345f5be8a1dd4f6394
harshitgupta2004 <harshitcba@gmail.com>
2023-10-24 10:15:47 +0530

    formatted

--- STRINGS/CommonPrefix.java
+++ STRINGS/CommonPrefix.java
@@ -1,23 +1,24 @@
-public class CommonPrefix{
+public class CommonPrefix {
     public static int commonPrefix(String input) {
         int n = input.length();
-        if(n == 0) return 0;
-    
+        if (n == 0)
+            return 0;
+
         int res = n;
         char[] arr = input.toCharArray();
-        for(int i = 1; i < n; i++) {
-            if(arr[i] == arr[0])
+        for (int i = 1; i < n; i++) {
+            if (arr[i] == arr[0])
                 res += getPrefixLength(arr, i);
         }
-    
+
         return res;
     }
-    
+
     public static int getPrefixLength(char[] arr, int start) {
         int res = 0, i = 0, j = start;
-    
-        while(j < arr.length) {
-            if(arr[i++] == arr[j++])
+
+        while (j < arr.length) {
+            if (arr[i++] == arr[j++])
                 res++;
             else
                 break;
