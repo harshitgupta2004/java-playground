a7b0643f98d68a968fe2156fd2c97e493a87038b
harshitgupta2004 <harshitcba@gmail.com>
2023-10-24 10:16:25 +0530

    fix formatting

--- STRINGS/PrintDuplicates.java
+++ STRINGS/PrintDuplicates.java
@@ -1,17 +1,18 @@
 public class PrintDuplicates {
-    public static void printDups(String s){
+    public static void printDups(String s) {
         int count[] = new int[256];
 
-        for(int i = 0 ; i < s.length(); i++){
+        for (int i = 0; i < s.length(); i++) {
             count[s.charAt(i)]++;
         }
 
-        for(int i = 0; i < 256; i++){
-            if(count[i] > 1){
-                System.out.println((char)(i) + ", count = " + count[i]);
+        for (int i = 0; i < 256; i++) {
+            if (count[i] > 1) {
+                System.out.println((char) (i) + ", count = " + count[i]);
             }
         }
     }
+
     public static void main(String[] args) {
         String str = "test string";
         printDups(str);
