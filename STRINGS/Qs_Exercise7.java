61a54182b52b92165765d61017a6dfbb7ea0e50f
harshitgupta2004 <harshitcba@gmail.com>
2023-10-24 10:15:39 +0530

    formatted

--- STRINGS/Qs_Exercise7.java
+++ STRINGS/Qs_Exercise7.java
@@ -4,53 +4,54 @@ import java.util.Scanner;
 // STRINGS
 public class Qs_Exercise7 {
     public static void main(String args[]) {
-        //Count how many times lowercase vowels occurred in a String entered by the user
+        // Count how many times lowercase vowels occurred in a String entered by the
+        // user
         // Scanner sc = new Scanner(System.in);
         // String lowercase = sc.nextLine();
-        // OR USE BELOW 
+        // OR USE BELOW
         String lowercase = new Scanner(System.in).nextLine();
         int count = 0;
 
-        for(int i = 0; i < lowercase.length(); i++){
+        for (int i = 0; i < lowercase.length(); i++) {
             char ch = lowercase.charAt(i);
-            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' ||ch == 'u'){
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
                 count++;
             }
         }
         System.out.println("Count of Vowels is: " + count);
 
-
-
         // Determine if two strings are ANAGRAMS of each other
-        // ANAGRAMS - If two strings contain the same characters but in a different order
+        // ANAGRAMS - If two strings contain the same characters but in a different
+        // order
         String str1 = "earth";
         String str2 = "heart";
 
-        // Convert Strings to lowercase. Why? so that we don't have to checkseparately for lower & uppercase
+        // Convert Strings to lowercase. Why? so that we don't have to checkseparately
+        // for lower & uppercase
         str1 = str1.toLowerCase();
         str2 = str2.toLowerCase();
 
         // First check - if the lenghts are the same
-        if(str1.length() == str2.length()){
-            //convert strings to char array
+        if (str1.length() == str2.length()) {
+            // convert strings to char array
             char[] str1charArray = str1.toCharArray();
             char[] str2charArray = str2.toCharArray();
 
-            //sort the char array
+            // sort the char array
             Arrays.sort(str1charArray);
             Arrays.sort(str2charArray);
 
-            //if the sorted char arrays are same or identical then the strings are anagram
+            // if the sorted char arrays are same or identical then the strings are anagram
             boolean Result = Arrays.equals(str1charArray, str2charArray);
-            if(Result){
+            if (Result) {
                 System.out.println(str1 + " and " + str2 + " are anagram");
-            } else{
+            } else {
                 System.out.println(str1 + " and " + str2 + " are not anagram");
             }
-        } else{
-            //when strings lengths not equal
+        } else {
+            // when strings lengths not equal
             System.out.println(str1 + " and " + str2 + " are not anagrams");
         }
 
-    }    
+    }
 }
--- STRINGS/Strings.java
+++ STRINGS/Strings.java
@@ -1,39 +1,40 @@
 import java.util.Scanner;
 
-public class Strings{
+public class Strings {
 
-    //Print all letters of a string
+    // Print all letters of a string
     public static void printLetters(String str) {
-        for(int i = 0; i < str.length(); i++){
-            System.out.print(str.charAt(i)+ " ");
+        for (int i = 0; i < str.length(); i++) {
+            System.out.print(str.charAt(i) + " ");
         }
     }
 
     public static void main(String args[]) {
-        char arr[] = {'a', 'b', 'c', 'd'}; //character array
-        
-        String str = "abcd"; //memory me value store hui abcd, jo ki string type ki hai
-        String str2 = new String("xyz"); //woth new keyword
+        char arr[] = { 'a', 'b', 'c', 'd' }; // character array
+
+        String str = "abcd"; // memory me value store hui abcd, jo ki string type ki hai
+        String str2 = new String("xyz"); // woth new keyword
         // both above are the methods to declare a string
 
-        //Strings in JAVA are IMMUTABLE
-        // means strings cannot be change in java, we cannot make any changes in the already created strings
+        // Strings in JAVA are IMMUTABLE
+        // means strings cannot be change in java, we cannot make any changes in the
+        // already created strings
 
         // INPUT/OUTPUT
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
-        //String name;
+        // String name;
         // name = sc.next();
         System.out.println("Name is: " + name);
 
         // String LENGTH
-        String fullname = "Harshit Gupta"; //space only het counted in length
+        String fullname = "Harshit Gupta"; // space only het counted in length
         System.out.println(fullname.length());
 
         // String CONCATENATION
         String firstname = "Harshit";
         String lastname = "Gupta";
-        String fullname2 = firstname + " " + lastname; //combing one string to the end of another string
+        String fullname2 = firstname + " " + lastname; // combing one string to the end of another string
         System.out.println(fullname2);
         // OR USE concat() method - but not use it as using + is more easy
         String s = "hello ";
@@ -42,7 +43,7 @@ public class Strings{
 
         // TO find a Character in a String using CharAt
         System.out.println(fullname2.charAt(9));
-        
+
         printLetters(fullname2);
 
         // String FUNCIONS - COMPARE
@@ -52,20 +53,20 @@ public class Strings{
         String s3 = new String("Joy");
 
         // if(s1 == s2){
-        //     System.out.println("Stings are equal");
+        // System.out.println("Stings are equal");
         // } else{
-        //     System.out.println("Strings are not equal");
+        // System.out.println("Strings are not equal");
         // }
 
         // if(s1 == s3){
-        //     System.out.println("Stings are equal");
+        // System.out.println("Stings are equal");
         // } else{
-        //     System.out.println("Strings are not equal");
+        // System.out.println("Strings are not equal");
         // }
 
-        if(s1.equals(s3)){ //equals function is used to compare Strings
+        if (s1.equals(s3)) { // equals function is used to compare Strings
             System.out.println("Equal");
-        } else{
+        } else {
             System.out.println("Not Equal");
         }
 
