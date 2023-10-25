ee33ed27b43a4fd74b90882f53e0850f27688da0
harshitgupta2004 <harshitcba@gmail.com>
2023-10-25 10:35:55 +0530

    proper format

--- ARRAYS/MO_Algorithm.java
+++ ARRAYS/MO_Algorithm.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
-class Query{
+class Query {
     int L;
     int R;
 
-    Query(int L, int R){
+    Query(int L, int R) {
         this.L = L;
         this.R = R;
     }
@@ -14,35 +14,37 @@ class Query{
 
 public class MO_Algorithm {
 
-    public static void querySum (int arr[], int n, ArrayList<Query> q, int m){
-        for(int i = 0; i < n; i++){
+    public static void querySum(int arr[], int n, ArrayList<Query> q, int m) {
+        for (int i = 0; i < n; i++) {
             // Left and right boundaries of current range
             int l = q.get(i).L;
             int r = q.get(i).R;
 
             int sum = 0;
-            for(int j = l; j <= r; j++){
+            for (int j = l; j <= r; j++) {
                 sum += arr[j];
             }
 
-            System.out.println("Sum of [" + l + ", " + r + "] is "  + sum);
+            System.out.println("Sum of [" + l + ", " + r + "] is " + sum);
         }
     }
 
-    public static void MO(int arr[], int n, ArrayList<Query> q, int m){
+    public static void MO(int arr[], int n, ArrayList<Query> q, int m) {
         int block = (int) Math.sqrt(n); // block size
 
-
-        /*Function used to sort all queries so that all queries of the same block are arranged together
-        and within a block, queries are sorted in increasing order of R values */
+        /*
+         * Function used to sort all queries so that all queries of the same block are
+         * arranged together
+         * and within a block, queries are sorted in increasing order of R values
+         */
         Collections.sort(q, new Comparator<Query>() {
-            public int compare(Query a, Query b){
+            public int compare(Query a, Query b) {
                 // Different blocks, sort by block
-                if(a.L/block != b.L/block){
+                if (a.L / block != b.L / block) {
                     return (a.L < b.L ? -1 : 1);
                 }
                 // Same block, sort by R value
-                else{
+                else {
                     return (a.R < b.R ? -1 : 1);
                 }
             }
@@ -51,42 +53,43 @@ public class MO_Algorithm {
         int currL = 0, currR = 0, sum = 0;
 
         // traverse through all queries
-        for(int i = 0; i < m; i++){
+        for (int i = 0; i < m; i++) {
 
             // l and r values of curr range
             int l = q.get(i).L;
             int r = q.get(i).R;
 
             // remove extra elms of previous range
-            while(currL < l){
+            while (currL < l) {
                 sum -= arr[currL];
                 currL++;
             }
 
-            while(currR > r+1){
+            while (currR > r + 1) {
                 sum -= arr[currR];
                 currR--;
             }
 
             // Add Elements of current Range
-            while(currL > l){
-                sum += arr[currL-1];
+            while (currL > l) {
+                sum += arr[currL - 1];
                 currL--;
             }
-            while(currR <= r){
+            while (currR <= r) {
                 sum += arr[currR];
                 currR++;
             }
-            System.out.println("Sum of [" + l + ", " + r + "] is "  + sum);
+            System.out.println("Sum of [" + l + ", " + r + "] is " + sum);
         }
     }
+
     public static void main(String[] args) {
-        int arr[] = {1, 1, 2, 1, 3, 4, 5, 2, 8};
+        int arr[] = { 1, 1, 2, 1, 3, 4, 5, 2, 8 };
         int n = arr.length;
         ArrayList<Query> q = new ArrayList<Query>();
-        q.add(new Query(0,4));
-        q.add(new Query(1,3));
-        q.add(new Query(2,4));
+        q.add(new Query(0, 4));
+        q.add(new Query(1, 3));
+        q.add(new Query(2, 4));
         int m = q.size();
 
         // querySum(arr, n, q, m);
--- ARRAYS/Pair_GivenSum.java
+++ ARRAYS/Pair_GivenSum.java
@@ -1,34 +1,33 @@
 // O(n); SC = O(1)
 public class Pair_GivenSum {
-    public static boolean findPair(int arr[], int k){
+    public static boolean findPair(int arr[], int k) {
         int n = arr.length;
-        
-        //find the pivot 
+
+        // find the pivot
         int pivot = 0;
-        for(int i = 0; i < n-1; i++){
-            if(arr[i] > arr[i+1]){
-                pivot = i+1;
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                pivot = i + 1;
             }
         }
 
         int l = pivot;
-        int r = pivot-1;
-        while(l != r){
-            if(arr[l] + arr[r] == k){
+        int r = pivot - 1;
+        while (l != r) {
+            if (arr[l] + arr[r] == k) {
                 return true;
-            }
-            else if(arr[l] + arr[r] < k){
-                l = (l+1)%n;
-            }
-            else{
-                r = (r-1+n)%n;
+            } else if (arr[l] + arr[r] < k) {
+                l = (l + 1) % n;
+            } else {
+                r = (r - 1 + n) % n;
             }
         }
 
         return false;
     }
+
     public static void main(String[] args) {
-        int[] arr = {11, 15, 6, 8, 9, 10};
+        int[] arr = { 11, 15, 6, 8, 9, 10 };
         int k = 16;
         System.out.println(findPair(arr, k));
     }
