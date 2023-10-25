c5f746ba363340952407efa1447f4639d3b2ad04
harshitgupta2004 <harshitcba@gmail.com>
2023-10-25 10:35:43 +0530

    proper format

--- ARRAYS/MinOps_For_Palindrome.java
+++ ARRAYS/MinOps_For_Palindrome.java
@@ -1,34 +1,35 @@
 public class MinOps_For_Palindrome {
-    public static int minOps(int arr[], int n){
+    public static int minOps(int arr[], int n) {
         int ans = 0;
         // start from two corners
-        for(int i = 0, j = n-1; i <= j;){
+        for (int i = 0, j = n - 1; i <= j;) {
 
             // If corner elements are same, problem reduces arr[i+1..j-1]
-            if(arr[i] == arr[j]){
+            if (arr[i] == arr[j]) {
                 i++;
                 j--;
             }
 
             // If left element is greater, then we merge right two elements
-            else if(arr[i] > arr[j]){
+            else if (arr[i] > arr[j]) {
                 // need to merge from tail
                 j--;
-                arr[j] += arr[j+1];
+                arr[j] += arr[j + 1];
                 ans++;
             }
 
             // Else we merge left two elements
-            else{
+            else {
                 i++;
-                arr[i] += arr[i-1];
+                arr[i] += arr[i - 1];
                 ans++;
             }
         }
         return ans;
     }
+
     public static void main(String[] args) {
-        int arr[] = {1, 4, 5, 9, 1} ;
+        int arr[] = { 1, 4, 5, 9, 1 };
         int n = arr.length;
         System.out.println(minOps(arr, n));
     }
